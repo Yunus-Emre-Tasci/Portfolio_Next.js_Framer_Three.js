e154e6d29e45a9b16d8da4f478ff27a7b4422c56
Yunus Emre Taşçı <yunusemretasci092@gmail.com>
2025-10-01 01:29:23 +0300

    Refactor code structure for improved readability and maintainability

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,7 +37,7 @@ const Footer = () => {
           Copyright © 2025 Yunus Emre Taşçı
         </p>
 
-        <div className="flex items-center md:gap-3 gap-6">
+        {/* <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
             <div
               key={info.id}
@@ -45,6 +45,22 @@ const Footer = () => {
             >
               <img src={info.img} alt="icons" width={20} height={20} />
             </div>
+          ))}
+        </div> */}
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((info) => (
+            <a
+              key={info.id}
+              href={info.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center 
+                 backdrop-filter backdrop-blur-lg saturate-180 
+                 bg-opacity-75 bg-black-200 rounded-lg 
+                 border border-black-300 hover:scale-110 transition"
+            >
+              <img src={info.img} alt="icon" width={20} height={20} />
+            </a>
           ))}
         </div>
       </div>
--- data/index.ts
+++ data/index.ts
@@ -74,7 +74,7 @@ export const projects = [
     id: 1,
     title: "3D Solar System Planets to Explore",
     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
+    img: "/StockApp.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
     link: "/ui.earth.com",
   },
@@ -82,7 +82,7 @@ export const projects = [
     id: 2,
     title: "Yoom - Video Conferencing App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
+    img: "/ExpenseTracker.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
     link: "/ui.yoom.com",
   },
@@ -90,7 +90,7 @@ export const projects = [
     id: 3,
     title: "AI Image SaaS - Canva Application",
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
+    img: "/NextAI.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
     link: "/ui.aiimg.com",
   },
@@ -98,7 +98,7 @@ export const projects = [
     id: 4,
     title: "Animated Apple Iphone 3D Website",
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
+    img: "/VuexMovie.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
     link: "/ui.apple.com",
   },
@@ -205,13 +205,17 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/Yunus-Emre-Tasci",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://twitter.com/", // şimdilik placeholder
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/yunus-emre-tasci/",
   },
 ];
+
